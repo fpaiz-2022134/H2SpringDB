5d41caa98e17548965c309bb29012e72600f3305
practicasmultigroup <practicasmultigroup@gmail.com>
2024-09-19 13:07:36 -0600

    Implementing H2 Database in spring boot web project.

---
+++ src/main/java/com/example/h2SpringDB/H2SpringDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2SpringDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2SpringDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2SpringDbApplication.class, args);
+	}
+
+}
